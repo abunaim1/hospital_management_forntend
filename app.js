de2faa4a2610559d7ebbb13ee4f894c065089793
Abu Naim <abunaim@Abus-MacBook-Air-2.local>
2024-02-09 10:42:07 +0600

    searchable

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const loadservices = () => {
+const loadServices = () => {
   fetch("https://testing-8az5.onrender.com/services/")
     .then((res) => res.json())
     .then((data) => displaydata(data))
@@ -6,7 +6,7 @@ const loadservices = () => {
 };
 
 const displaydata = (services) => {
-  console.log(services);
+  //   console.log(services);
   services.forEach((service) => {
     const parent = document.getElementById("service-container");
     const li = document.createElement("li");
@@ -17,7 +17,7 @@ const displaydata = (services) => {
     </div>
     <div class="card-body p-3 p-xl-5">
       <h3 class="card-title h5">${service.name}</h3>
-      <p class="card-text">${service.description.slice(0,100)}</p>
+      <p class="card-text">${service.description.slice(0, 100)}</p>
       <a href="#" class="btn btn-primary">Details</a>
     </div>
   </div>
@@ -25,4 +25,64 @@ const displaydata = (services) => {
     parent.appendChild(li);
   });
 };
-loadservices();
+const loadDoctors = () => {
+  fetch("https://testing-8az5.onrender.com/doctor/list/")
+    .then((res) => res.json())
+    .then((data) => displayDoctors(data?.results));
+};
+const displayDoctors = (doctors) => {
+  doctors?.forEach((doctor) => {
+    const parent = document.getElementById("doctors");
+    const div = document.createElement("div");
+    div.classList.add("doctor");
+    div.innerHTML = `
+    <img src=${doctor.image} class="card-img-top" alt="Doctor image" />
+    <div class="card-body">
+      <h4 class="card-title">${doctor?.full_name}</h4>
+      <h5>${doctor?.designation[0]}</h5>
+      <p>Beyond just treating symptoms,we believe in addressing the root causes of illness and promoting holistic well-being.</p>
+      ${doctor?.specialization?.map((item) => {
+        return `<span class=specialization>${item}</span>`;
+      })}
+      <a href="#" class="btn btn-primary">Details</a>
+    </div>
+    `;
+    parent.appendChild(div);
+  });
+};
+
+const loadDesignation = () => {
+  fetch("https://testing-8az5.onrender.com/doctor/designation/")
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
+      const parent = document.getElementById("designation");
+      data.forEach((item) => {
+        const li = document.createElement("li");
+        li.classList.add("dropdown-item");
+        li.innerText = `${item?.name}`;
+        parent.appendChild(li);
+      });
+    });
+};
+
+const loadSpecialization = () => {
+    fetch("https://testing-8az5.onrender.com/doctor/specialization/")
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        const parent = document.getElementById("specialization");
+        data.forEach((item) => {
+          const li = document.createElement("li");
+          li.classList.add("dropdown-item");
+          li.innerText = `${item?.name}`;
+          parent.appendChild(li);
+        });
+      });
+  };
+
+
+loadServices();
+loadDoctors();
+loadDesignation();
+loadSpecialization()
